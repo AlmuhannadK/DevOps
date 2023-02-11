f3668ae9430c5f2b6aa06ae1e670a2ecf465e71c
Almuhannad Alotaibi <almuhannad.khalid@gmail.com>
2023-02-11 16:03:56 +0300

    Added for loop to the file

--- demo222.java
+++ demo222.java
@@ -1,8 +1,11 @@
 public class demo
 {
-	public static void main(String[] args){
+	public static void main(String[] args)
+	{
 
-	System.out.println("Welcome to Java");
+		for(int i=0; i<10; i++){
+		  System.out.println("Welcome to Java");
+		}
 	
 	
 
