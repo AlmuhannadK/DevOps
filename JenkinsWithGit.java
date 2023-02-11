c0e2fc0d9f9f048afdb0a33a110500fe353568c6
Almuhannad Alotaibi <almuhannad.khalid@gmail.com>
2023-02-11 17:13:23 +0300

    Pushing JenkinsWithGit.java

---
+++ JenkinsWithGit.java
@@ -0,0 +1,11 @@
+public class JenkinsWithGit {
+    public static void main(String[] args) {
+        
+        int charPerLine = 0;
+
+        for(int i = 33; i < 256; i++; charPerLine++){
+            System.out.println((char)i+((charPerLine%10==0)?"\n":"\t"));
+            System.out.println();
+        }
+    }
+}
--- demo222.java
+++ demo222.java
@@ -1,11 +1,8 @@
 public class demo
 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
 
-		for(int i=0; i<10; i++){
-		  System.out.println("Welcome to Java");
-		}
+	System.out.println("Welcome to Java");
 	
 	
 
